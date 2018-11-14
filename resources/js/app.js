320c41e8b38efb71f98d0b66b56284d7c7bfecef
Gabe Landau <gabelandau14@gmail.com>
2018-11-14 12:50:48 -0500

    Cleaned up some files

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,39 +1,15 @@
+import './bootstrap'
 
-/**
- * First we will load all of this project's JavaScript dependencies which
- * includes Vue and other libraries. It is a great starting point when
- * building robust, powerful web applications using Vue and Laravel.
- */
+window.Vue = require('vue')
 
-require('./bootstrap');
+window.Vue.component('message-field', require('./components/MessageField.vue'))
+window.Vue.component('messages', require('./components/Messages.vue'))
+window.Vue.component('lobby', require('./components/Lobby.vue'))
+window.Vue.component('game-header', require('./components/game/GameHeader.vue'))
+window.Vue.component('board', require('./components/game/Board.vue'))
+window.Vue.component('piece', require('./components/game/Piece.vue'))
+window.Vue.component('cell-table', require('./components/game/CellTable.vue'))
 
-window.Vue = require('vue');
-
-/**
- * The following block of code may be used to automatically register your
- * Vue components. It will recursively scan this directory for the Vue
- * components and automatically register them with their "basename".
- *
- * Eg. ./components/ExampleComponent.vue -> <example-component></example-component>
- */
-
-Vue.component('message-field', require('./components/MessageField.vue'));
-Vue.component('messages', require('./components/Messages.vue'));
-Vue.component('lobby', require('./components/Lobby.vue'));
-Vue.component('game-header', require('./components/game/GameHeader.vue'));
-
-// const files = require.context('./', true, /\.vue$/i)
-
-// files.keys().map(key => {
-//     return Vue.component(_.last(key.split('/')).split('.')[0], files(key))
-// })
-
-/**
- * Next, we will create a fresh Vue application instance and attach it to
- * the page. Then, you may begin adding components to this application
- * or customize the JavaScript scaffolding to fit your unique needs.
- */
-
-const app = new Vue({
-    el: '#app'
-});
+new window.Vue({ // eslint-disable-line no-new
+  el: '#app'
+})
